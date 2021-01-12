a136678f42aa14347936483edb47a4f11c857edb
Sergio Rios <sergiorios@MacBook-de-Sergio.local>
2021-01-12 13:50:14 -0600

    deployment-seguro

--- config/db.js
+++ config/db.js
@@ -1,10 +1,18 @@
 const Sequelize = require('sequelize');
 require('dotenv').config({path:'variables.env'});
+var pg = require('pg');
+pg.defaults.ssl = true;
 
 module.exports = new Sequelize(process.env.BD_NOMBRE,process.env.BD_USER,process.env.BD_PASS,{
     host: process.env.BD_HOST,
     port: process.env.BD_PORT,
     dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+        }
+      },
     pool: {
         max:5,
         min:0,
@@ -14,5 +22,5 @@ module.exports = new Sequelize(process.env.BD_NOMBRE,process.env.BD_USER,process
     define:{
         timestamps:false
     },
-    logging:false,
+    ssl: true
 });
